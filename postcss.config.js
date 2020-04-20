60ee8cb0cc781c37814e348743e5cfa9f9fb3d45
vibhanshu <vibhanshu909@gmail.com>
2020-04-20 17:22:45 +0530

    Feature
    added purgecss plugin to reduce build css size, also remove unused dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,21 @@
+const purgecss = [
+  '@fullhuman/postcss-purgecss',
+  {
+    // Specify the paths to all of the template files in your project
+    content: ['./Layout/**/*.tsx', './pages/**/*.tsx', './components/**/*.tsx'],
+
+    // make sure css reset isnt removed on html and body
+    // Using react-icons/fa which vomets svg
+    whitelist: ['html', 'body', 'svg'],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+]
 module.exports = {
-  plugins: ['tailwindcss', 'autoprefixer', 'cssnano'],
+  plugins: [
+    'tailwindcss',
+    'autoprefixer',
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 }
